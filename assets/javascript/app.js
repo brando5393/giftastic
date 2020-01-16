91336fd20bef4ce977a2b67c22336d5dfb126f30
Brandon Williams <brando5393@gmail.com>
2020-01-15 22:02:33 -0600

    user buttons displaying

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,21 @@
-var buttonsArray = ["time lord","cyberman","dalek","tardis","doctor who"];
-var userSearchTerm = $("#gif-search-box").val();
+var defaultButtonsArray = ["time lord","cyberman","dalek","tardis","doctor who"];
+var userSearches = [];
 $(document).ready(function(){
-    for(i = 0; i < buttonsArray.length; i++){
-        var buttonText = buttonsArray[i];
-        var searchResultButton = $("<button>").addClass("btn btn-info search-result-btn ").attr('type', 'button',).prop('value', buttonText).text(buttonText);
+    for(i = 0; i < defaultButtonsArray.length; i++){
+        var buttonText = defaultButtonsArray[i];
+        var searchResultButton = $("<button>").addClass("btn btn-info search-result-btn").attr('type', 'button',).prop('value', buttonText).text(buttonText);
         $("#button-container").append(searchResultButton);
     }
-
+    
     $("#search-btn").on("click", function(event){
-        event.preventDefault();
-        userSearchResultButton = $("<button>").addClass("btn btn-info search-result-btn ").attr('type', 'button',).prop('value', userSearchTerm).text(userSearchTerm);
-        $("#button-container").append(userSearchResultButton);
-        $("#gif-search-box").val("");
+        var userSearchTerm = $("#gif-search-box").val();
+            event.preventDefault();
+            var userButtonText = userSearchTerm
+            var userSearchResultButton = $("<button>").addClass("btn btn-info search-result-btn ").attr('type', 'button',).prop('value', userButtonText).text(userButtonText);
+            console.log(userButtonText);
+            $("#button-container").append(userSearchResultButton);
+            $("#gif-search-box").val("");
 
-    })
+
+        })
 });
